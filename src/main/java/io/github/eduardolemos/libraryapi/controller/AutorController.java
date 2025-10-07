949f07efa1a7c63afe2fc49f6d4ea5a95d229339
EduardoLMartins <eduardol_martins@hotmail.com>
2025-10-06 22:32:59 -0300

    Implementacao das anotacoes Size e Past

--- src/main/java/io/github/eduardolemos/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/eduardolemos/libraryapi/controller/AutorController.java
@@ -107,7 +107,7 @@ public class AutorController {
 	}
 	
 	@PutMapping("{id}")
-	public ResponseEntity<Object> atualizar(@PathVariable("id") String id, @RequestBody AutorDTO dto) {
+	public ResponseEntity<Object> atualizar(@PathVariable("id") @Valid String id, @RequestBody AutorDTO dto) {
 
 		try {
 		var idAutor = UUID.fromString(id);
--- src/main/java/io/github/eduardolemos/libraryapi/dto/AutorDTO.java
+++ src/main/java/io/github/eduardolemos/libraryapi/dto/AutorDTO.java
@@ -6,18 +6,23 @@ import java.util.UUID;
 import io.github.eduardolemos.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 public record AutorDTO(
 		
 		UUID id,
 		
 		@NotBlank(message = "campo obrigatorio")
+		@Size(min = 2, max = 100, message = "campo fora do tamanho padrao")
 		String nome,
 		
 		@NotNull(message = "campo obrigatorio")
-		LocalDate dataNascimento, 
+		@Past(message = "nao pode ser uma data futura")
+		LocalDate dataNascimento,
 		
 		@NotBlank(message = "campo obrigatorio")
+		@Size(max = 50, min = 2, message = "campo fora do tamanho padrao")
 		String nacionalidade) {
 
 
